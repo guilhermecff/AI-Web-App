fdc594538f25e92689bc3be5cb0badd19f1ade20
Guilherme Fonseca <guiga.froes3@gmail.com>
2024-08-14 22:40:16 -0300

    More authentication

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,12 +1,28 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 
+
+
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username= username, password= password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {"error_message": error_message})
     return render(request, 'login.html')
 
 def user_signup(request):
@@ -34,4 +50,5 @@ def user_signup(request):
 
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')
